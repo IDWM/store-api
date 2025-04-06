3cf11e8bce287de90d140079225f5b74af017841
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-04-05 23:39:29 -0400

    feat: run database migrations on startup

--- Program.cs
+++ Program.cs
@@ -7,4 +7,19 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlite("Data So
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocurrió un error al poblar la base de datos.");
+    }
+}
+
 app.Run();
