b60d9ea9df00002c6865c524c90ba57b6ef19d2f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-04-05 23:32:21 -0400

    feat: create product and store models

---
+++ Src/Models/Product.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace store_api.Src.Models
+{
+    public class Product
+    {
+        [Key]
+        public required int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public required string Name { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public required float Price { get; set; }
+
+        public required int StoreId { get; set; }
+        public required Store Store { get; set; }
+    }
+}
---
+++ Src/Models/Store.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace store_api.Src.Models
+{
+    public class Store
+    {
+        [Key]
+        public required int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public required string Name { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public required string City { get; set; }
+
+        public List<Product> Products { get; set; } = [];
+    }
+}
